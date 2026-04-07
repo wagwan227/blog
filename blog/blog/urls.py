4040617d12ebf9636fa1da4e9bd95f373926bbff
Vitaly <fujutoide@gmail.com>
2026-04-07 12:03:32 +0700

    добавлен маршрут для создания пользователя

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 #from django.contrib import admin
 from django.urls import path
+from login import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
+    path('add_user/', views.add_user),
 ]
